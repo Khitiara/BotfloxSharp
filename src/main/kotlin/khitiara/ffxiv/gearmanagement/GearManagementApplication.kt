651469e0bc58cfa4478bc6ace83fdea05de9eee9
Lily Sieberer <robotbrainify@gmail.com>
2020-05-30 02:33:33 -0400

    Initial Commit
    Signed-off-by: Lily Sieberer <robotbrainify@gmail.com>

---
+++ src/main/kotlin/khitiara/ffxiv/gearmanagement/GearManagementApplication.kt
@@ -0,0 +1,11 @@
+package khitiara.ffxiv.gearmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GearManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<GearManagementApplication>(*args)
+}
---
+++ src/test/kotlin/khitiara/ffxiv/gearmanagement/GearManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package khitiara.ffxiv.gearmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GearManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

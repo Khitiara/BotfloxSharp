f8a6197082af21ecea36131ebf444615df904d80
Lily Sieberer <robotbrainify@gmail.com>
2020-06-03 21:02:49 -0400

    whoops
    Signed-off-by: Lily Sieberer <robotbrainify@gmail.com>

--- src/main/kotlin/khitiara/ffxiv/gearmanagement/Ext.kt
+++ src/main/kotlin/khitiara/ffxiv/gearmanagement/Ext.kt
@@ -1,15 +1,8 @@
 package khitiara.ffxiv.gearmanagement
 
-import io.lettuce.core.RedisURI
-import org.springframework.data.redis.connection.RedisPassword
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-
 fun String.toSlug() = toLowerCase()
     .replace("\n", " ")
     .replace("[^a-z\\d\\s]".toRegex(), " ")
     .split(" ")
     .joinToString("-")
     .replace("-+".toRegex(), "-")
-
-fun RedisURI.toSpringConfig(): RedisStandaloneConfiguration =
-    RedisStandaloneConfiguration(host, port).also { it.password = RedisPassword.of(password) }

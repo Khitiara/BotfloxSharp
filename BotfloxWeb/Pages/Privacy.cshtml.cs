0a75964d8d257695ab80a707791a820be80f8624
Khitiara <robotbrainify@gmail.com>
2020-10-01 20:59:55 -0400

    Web app part

---
+++ BotfloxWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BotfloxWeb.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger) {
+            _logger = logger;
+        }
+
+        public void OnGet() { }
+    }
+}

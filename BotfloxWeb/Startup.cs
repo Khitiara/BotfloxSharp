8a06d317f197f610cc1eeae7634f80a64c42d72e
Khitiara <robotbrainify@gmail.com>
2020-10-05 15:17:46 -0400

    Move basic endpoints out of redundant page files

--- BotfloxWeb/Startup.cs
+++ BotfloxWeb/Startup.cs
@@ -1,12 +1,22 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net.Mime;
+using System.Threading.Tasks;
 using AspNet.Security.OAuth.Discord;
 using Botflox.Bot;
+using Botflox.Bot.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using XivApi;
+using XivApi.Character;
 
 namespace BotfloxWeb
 {
@@ -35,11 +45,11 @@ namespace BotfloxWeb
             services.AddAuthorization(options => {
                 // Policies here
             });
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
+            services.Configure<ForwardedHeadersOptions>(options => {
                 options.ForwardedHeaders =
                     ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
             });
+            services.Configure<RouteOptions>(options => { options.LowercaseUrls = true; });
             BotfloxInit.ConfigureServices(services);
         }
 
@@ -62,7 +72,37 @@ namespace BotfloxWeb
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
+            app.UseEndpoints(endpoints => {
+                endpoints.MapGet("/discord", async context => {
+                    context.Response.Redirect("https://discord.gg/NvQ5Udx");
+                    await context.Response.CompleteAsync();
+                });
+                endpoints.MapGet("/invite", async context => {
+                    BotfloxBot bot = context.RequestServices.GetRequiredService<BotfloxBot>();
+                    context.Response.Redirect((await bot.GetInviteUriAsync()).ToString());
+                    await context.Response.CompleteAsync();
+                });
+                endpoints.MapGet("/profile/by-lodestone/{id}", async context => {
+                    IServiceProvider sp = context.RequestServices;
+                    HttpResponse response = context.Response;
+                    if (!ulong.TryParse(context.Request.RouteValues["id"] as string, out ulong lodestoneId)) {
+                        response.StatusCode = StatusCodes.Status400BadRequest;
+                        await response.CompleteAsync();
+                        return;
+                    }
+
+                    XivApiClient xivApiClient = sp.GetRequiredService<XivApiClient>();
+                    CharacterProfileGeneratorService profileGeneratorService =
+                        sp.GetRequiredService<CharacterProfileGeneratorService>();
+
+                    CharacterProfile profile =
+                        await xivApiClient.CharacterProfileAsync(lodestoneId, context.RequestAborted);
+                    Image profileImage = await profileGeneratorService.RenderCharacterProfileAsync(profile);
+                    response.ContentType = "image/png";
+                    await Task.Run(() => profileImage.Save(response.Body, ImageFormat.Png), context.RequestAborted);
+                });
+                endpoints.MapRazorPages();
+            });
         }
     }
 }

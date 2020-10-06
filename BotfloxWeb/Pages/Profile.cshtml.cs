d853025d35c5456714d4933af6884c779defa57f
Khitiara <robotbrainify@gmail.com>
2020-10-06 16:11:05 -0400

    Only do api request once for profile page

--- BotfloxWeb/Pages/Profile.cshtml.cs
+++ BotfloxWeb/Pages/Profile.cshtml.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
 using Botflox.Bot.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
 using XivApi;
 using XivApi.Character;
 
@@ -9,16 +13,23 @@ namespace BotfloxWeb.Pages
 {
     public class Profile : PageModel
     {
-        private readonly XivApiClient _xivApiClient;
+        private readonly XivApiClient                     _xivApiClient;
+        private readonly CharacterProfileGeneratorService _profileGenerator;
 
-        public Profile(XivApiClient xivApiClient) {
+        public Profile(XivApiClient xivApiClient, CharacterProfileGeneratorService profileGenerator) {
             _xivApiClient = xivApiClient;
+            _profileGenerator = profileGenerator;
         }
 
         public CharacterProfile CharProfile { get; set; } = null!;
+        public string ProfileImageB64 { get; set; } = null!;
 
         public async Task OnGetAsync(ulong lodestoneId) {
             CharProfile = await _xivApiClient.CharacterProfileAsync(lodestoneId, HttpContext.RequestAborted);
+            Image image = await _profileGenerator.RenderCharacterProfileAsync(CharProfile);
+            await using MemoryStream memoryStream = new MemoryStream();
+            await Task.Run(() => image.Save(memoryStream, ImageFormat.Png));
+            ProfileImageB64 = Convert.ToBase64String(memoryStream.GetBuffer());
         }
     }
 }

e381a0a475767163a495863033c7d2c7325780a8
Khitiara <robotbrainify@gmail.com>
2020-10-09 02:07:51 -0400

    Suppress some warnings i missed

--- XivApi/Status/StatusDataCenter.cs
+++ XivApi/Status/StatusDataCenter.cs
@@ -4,9 +4,9 @@ namespace XivApi.Status
 {
     public class StatusDataCenter    {
         [JsonPropertyName("name")]
-        public string Name { get; set; } 
+        public string Name { get; set; } = null!;
 
         [JsonPropertyName("region")]
-        public string Region { get; set; } 
+        public string Region { get; set; } = null!;
     }
 }

33e86ee2ad3635c72f7177ddd4aa5d7199ef3006
Khitiara <robotbrainify@gmail.com>
2020-10-20 14:24:34 -0400

    Add bot module with links to site and invite

---
+++ DiscordBot/Modules/LinksModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Botflox.Bot.Modules
+{
+    public class LinksModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly BotfloxBot _bot;
+
+        public LinksModule(BotfloxBot bot) {
+            _bot = bot;
+        }
+
+        [Command("botflox")]
+        public async Task BotfloxWebsiteAsync() {
+            await ReplyAsync("More info about Botflox can be found at https://botflox.catboi.world");
+        }
+
+        [Command("invite")]
+        public async Task BotfloxInviteAsync() {
+            Uri invite = await _bot.GetInviteUriAsync();
+            await ReplyAsync(invite.ToString());
+        }
+    }
+}
